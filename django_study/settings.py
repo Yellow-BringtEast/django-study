afe6dfe1a7d2228d3ace0a8672f19b58e27fb0b9
Yellow-BringtEast <714376214@qq.com>
2022-10-02 12:40:49 +0800

    fix(settings.py):delete path of templates generated by pycharm.

--- django_study/settings.py
+++ django_study/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'django_study.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
